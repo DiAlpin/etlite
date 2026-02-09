0e5b6a2f0fa295a5c8deddfb2b5ae8a673e2d274
Daniel <daniel.broboana@gmail.com>
2026-02-09 22:40:08 +0200

    feat: add IndexWeightsYFExtractorModel class

--- src/etlite/extractors/__init__.py
+++ src/etlite/extractors/__init__.py
@@ -2,6 +2,7 @@
 from etlite.extractors.ext_s3 import S3ExtractorModel
 from etlite.extractors.ext_html import HtmlExtractorModel
 from etlite.extractors.ext_local import LocalExtractorModel
+from etlite.extractors.ext_yfinance import IndexWeightsYFExtractorModel
 from etlite.extractors.ext_tradingview import (
     OhlcTradingviewExtractorModel,
     LastPriceTradingviewExtractorModel,
@@ -15,6 +16,7 @@ __all__ = [
     "S3ExtractorModel",
     "HtmlExtractorModel",
     "LocalExtractorModel",
+    "IndexWeightsYFExtractorModel",
     "OhlcTradingviewExtractorModel",
     "LastPriceTradingviewExtractorModel",
     "PickDayPriceTradingviewExtractorModel",
